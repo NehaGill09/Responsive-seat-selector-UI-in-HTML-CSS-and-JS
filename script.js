db33bfbda8b05716604b4ebe88330d9ce6633543
neha gill <132168058+NehaGill09@users.noreply.github.com>
2024-02-16 14:39:06 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const seats = document.querySelectorAll('.seat');
+
+  seats.forEach(seat => {
+    seat.addEventListener('click', function() {
+      seat.classList.toggle('selected');
+    });
+  });
+});
